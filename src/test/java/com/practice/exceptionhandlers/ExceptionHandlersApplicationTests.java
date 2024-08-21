97e7eff9f9fb22ae1fec95e00b515c30a6094b3f
= <=>
2024-08-21 17:44:12 +0500

    init

---
+++ src/test/java/com/practice/exceptionhandlers/ExceptionHandlersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.exceptionhandlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
